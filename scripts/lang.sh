958058215fa329b4d3185ac20cca256ce50fcfb5
Batuhan Yetgin <byetgin@engineer.com>
2023-12-04 03:37:27 +0300

    localization script added

--- scripts/lang.sh
+++ scripts/lang.sh
@@ -1,1 +1,1 @@
-dart run easy_localization:generate -S assets/translations -f keys -o locale_keys.g.dart 
+dart run easy_localization:generate -S assets/translations -f keys -o locale_keys.g.dart -O lib/core/localization/generated
